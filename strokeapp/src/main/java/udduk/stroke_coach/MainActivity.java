3f05d1741230104d3e543ab94fb14861da9fd255
Andi <andipabst25@gmail.com>
2022-05-02 22:53:09 +0200

    convert to Android-X

--- strokeapp/src/main/java/udduk/stroke_coach/MainActivity.java
+++ strokeapp/src/main/java/udduk/stroke_coach/MainActivity.java
@@ -18,8 +18,8 @@ package udduk.stroke_coach;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.Toast;
 
--- strokeapp/src/main/java/udduk/stroke_coach/settings/SettingsActivity.java
+++ strokeapp/src/main/java/udduk/stroke_coach/settings/SettingsActivity.java
@@ -17,8 +17,8 @@
 package udduk.stroke_coach.settings;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 import udduk.stroke_coach.R;
 
